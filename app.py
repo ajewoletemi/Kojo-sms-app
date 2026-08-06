1b9e5ee5630a2969c325108e7d1737bf710b03e9
ajewoletemi <136252402+ajewoletemi@users.noreply.github.com>
2026-08-06 23:08:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+    import os
+    if os.path.exists('kojo.db'):
+        os.remove('kojo.db') # THIS DELETES OLD DB ON STARTUP
+        from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 import os
 import requests
